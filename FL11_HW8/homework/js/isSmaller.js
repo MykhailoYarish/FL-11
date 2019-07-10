0f2bc250ad79fca8a1c0334c19be63493df181b1
Mykhailo Yarish <mickhaelyarish@gmail.com>
2019-07-10 19:37:51 +0300

    Update isSmaller.js

--- FL11_HW8/homework/js/isSmaller.js
+++ FL11_HW8/homework/js/isSmaller.js
@@ -1,8 +1,9 @@
-function isSmaller(numberToCompare, comparingValue){
-  function isBigger(numberToCompare, comparingValue){
+function isSmaller(numberToCompare, comparingValue) {
+  function isBigger(numberToCompare, comparingValue) {
     return numberToCompare > comparingValue;
   }
+
   return !isBigger(numberToCompare, comparingValue);
 }
 
-isSmaller(5,-1)
+isSmaller(5, -1);
