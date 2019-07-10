6a84d1239c020b3395f02edc6d66ec6d030d30ec
Mykhailo Yarish <mickhaelyarish@gmail.com>
2019-07-10 19:36:19 +0300

    Update formatTime.js

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,15 +1,18 @@
-function formatTime(inputedMinutes){
- if(!Number.isInteger(inputedMinutes) || inputedMinutes < 0){
-     return false;
- }
- const minutesInDay = 1440;
- const minutesInHour = 60;
+function formatTime(inputedMinutes) {
+  if (inputedMinutes < 0) {
+    return false;
+  }
 
- const days = parseInt(inputedMinutes / minutesInDay);
- const hours = parseInt((inputedMinutes - days * minutesInDay)/minutesInHour );
- const minutes = (inputedMinutes) % 60;
- const time = `${days} day(s) ${hours} hour(s) ${minutes} minute(s).`;
- return time;
+  const minutesInDay = 1440;
+  const minutesInHour = 60;
+  const days = parseInt(inputedMinutes / minutesInDay);
+  const hours = parseInt(
+    (inputedMinutes - days * minutesInDay) / minutesInHour
+  );
+  const minutes = inputedMinutes % 60;
+  const time = `${days} day(s) ${hours} hour(s) ${minutes} minute(s).`;
+
+  return time;
 }
 
 formatTime(120);
